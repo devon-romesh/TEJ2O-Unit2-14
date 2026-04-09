e26e6927d13d9600f110da4da5ad71124ea14633
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-04-09 17:11:41 +0000

    coded working version 1.0

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,9 +1,90 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Devon
+Created on: Apr 2026
+This module is a Micro:bit MicroPython program that displays a pixel rotating and making a whole circle 
 """
 
 from microbit import *
 
-display.scroll("Hello, World!")
+# variables
+loopCounterX = 0
+loopCounterY = 0
+
+# setup
+display.clear()
+display.show(Image.HAPPY)
+while True:
+
+    # when A's pressed, the pixels move clock wise
+    if button_a.was_pressed():
+
+        # setup
+        display.clear()
+        loopCounterX = 0
+        loopCounterY = 0
+
+        # goes left to right
+        while loopCounterX < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX += 1
+
+        # goes up to down
+        while loopCounterY < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY += 1
+
+        # goes right to left
+        while loopCounterX > 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX -= 1
+
+        # goes down to up
+        while loopCounterY >= 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY -= 1
+        display.show(Image.HAPPY)
+
+    # when B's pressed, the pixels move counter clock wise
+    if button_b.was_pressed():
+
+        # setup
+        display.clear()
+        loopCounterX = 0
+        loopCounterY = 0
+
+        #  goes up to down
+        while loopCounterY < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY = loopCounterY + 1
+
+        #  goes left to right 
+        while loopCounterX < 4:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX = loopCounterX + 1
+
+        #  goes down to up
+        while loopCounterY > 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterY = loopCounterY - 1
+
+        # goes right to left
+        while loopCounterX >= 0:
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 9)
+            sleep(500)
+            display.set_pixel(0 + loopCounterX, 0 + loopCounterY, 0)
+            loopCounterX = loopCounterX - 1
+        display.show(Image.HAPPY)
