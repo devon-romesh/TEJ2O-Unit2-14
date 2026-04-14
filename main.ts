fa00bec748f2ba53997c1f53b526e375f1087790
devon-romesh <devon.romesh@stu.ocsb.ca>
2026-04-14 13:13:22 -0400

    code

--- main.ts
+++ main.ts
@@ -1,8 +1,92 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Devon
+ * Created on: Apr 2026
+ * This program makes a whole circle with one pixel
 */
 
-basic.showString('Hello, World!')
+let sprite: game.LedSprite = null
+let loopCounterX = 0
+let loopCounterY = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// when A's pressed, the pixels move clockwise
+input.onButtonPressed(Button.A, function () {
+
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    // goes left to right
+    while (loopCounterY <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounterY)
+        loopCounterY += 1
+        basic.pause(350)
+    }
+
+    // goes up to down
+    while (loopCounterX <= 4) {
+        sprite.set(LedSpriteProperty.Y, loopCounterX)
+        loopCounterX += 1
+        basic.pause(350)
+
+    }
+
+    // goes right to left
+    while (loopCounterY >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounterY)
+        loopCounterY -= 1
+        basic.pause(350)
+    }
+
+    // goes down to up
+    while (loopCounterX >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounterX)
+        loopCounterX -= 1
+        basic.pause(350)
+    }
+
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
+
+// when B's pressed, the pixels move counter clock wise
+input.onButtonPressed(Button.B, function () {
+
+    // setup
+    basic.clearScreen()
+    sprite = game.createSprite(0, 0)
+
+    // goes up to down
+    while (loopCounterX <= 4) {
+        sprite.set(LedSpriteProperty.Y, loopCounterX)
+        loopCounterX += 1
+        basic.pause(350)
+    }
+
+    // goes left to right 
+    while (loopCounterY <= 4) {
+        sprite.set(LedSpriteProperty.X, loopCounterY)
+        loopCounterY += 1
+        basic.pause(350)
+    }
+
+    // goes down to up
+    while (loopCounterX >= 0) {
+        sprite.set(LedSpriteProperty.Y, loopCounterX)
+        loopCounterX -= 1
+        basic.pause(350)
+    }
+
+    // goes right to left
+    while (loopCounterY >= 0) {
+        sprite.set(LedSpriteProperty.X, loopCounterY)
+        loopCounterY -= 1
+        basic.pause(350)
+    }
+    sprite.delete()
+    basic.showIcon(IconNames.Happy)
+})
